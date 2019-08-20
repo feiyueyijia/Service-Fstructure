e7436b876f4c0711a5ef3bdd025732cc404a5e43
jisongZhou <568777035@qq.com>
2019-08-20 15:41:13 +0800

    修正代码

--- src/main/java/com/yfny/servicefstructure/ServiceFStructureApplication.java
+++ src/main/java/com/yfny/servicefstructure/ServiceFStructureApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,18 +15,6 @@ public class ServiceFStructureApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceFStructureApplication.class, args);
-    }
-
-    @RestController
-    class EchoController {
-
-        @Value("${hello.name}")
-        private String name;
-
-        @GetMapping(value = "/echo/{string}")
-        public String echo(@PathVariable String string) {
-            return name + " Say Hello Nacos Discovery " + string;
-        }
     }
 
 }
